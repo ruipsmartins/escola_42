d93de68bcbef5e099eb4945346813b46aaf04d0b
Rui Martins <rui_pedru@hotmail.com>
2023-09-05 17:16:14 +0100

    update atoi

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,15 +1,42 @@
-#include <stdio.h>
-/*int ft_atoi(char *a)
-{
 
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	res;
+
+	i = 0;
+	sign = 1;
+	res = 0;
+	while ((str[i] >= 7 && str[i] <= 13) || str[i] == ' ')
+	{
+		i++;
+	}
+	while (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+		{
+			sign *= -1;
+		}
+		i++;
+	}
+	while (str[i] >= '0' && str [i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
 }
-*/
+
+#include <stdio.h>
+
 int	main(int ac, char **av)
 {
 	int	nbr;
 
-	if (ac > 1)
+	if (ac == 2)
 	{
+		nbr = ft_atoi(av[1]);
 		printf("%d\n", nbr);
 	}
 	return (0);
