f897542c40a6dc421ad289aac14c64cc2e38abd2
Rui Pedro Dos Santos Martins <ruidos-s@c1r17s11.42lisboa.com>
2023-10-04 17:08:36 +0100

    main

--- libft/main.c
+++ libft/main.c
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 15:34:41 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/04 17:00:54 by ruidos-s         ###   ########.fr       */
+/*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
+/*   Updated: 2023/10/04 17:05:38 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+#include <stdio.h>
 #include "libft.h"
 
-int	ft_isalnum(int c)
+int	main(void)
 {
-	if (ft_isalpha(c) || ft_isdigit(c))
-		return (1);
+	printf("%d\n", ft_isalnum('5'));
 	return (0);
 }
-/* 
- #include <stdio.h>
-
-int	main()
-{
-		printf("%d\n", ft_isalnum('5'));
-}
-    */
