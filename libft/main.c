407494c119ae16ebdb5306feca1f9863d6850ca3
Rui Pedro Dos Santos Martins <ruidos-s@c1r17s11.42lisboa.com>
2023-10-04 17:20:02 +0100

    asd

--- libft/main.c
+++ libft/main.c
@@ -6,10 +6,9 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/04 17:05:38 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/04 17:09:05 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include <stdio.h>
 #include "libft.h"
