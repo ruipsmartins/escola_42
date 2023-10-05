19f77ad94faf87ba6730a147c4908af4b0c06a95
ruipsmartins <rui_pedru@hotmail.com>
2023-10-05 10:45:58 +0100

    criar memset.c e alterar makefile

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,24 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/05 10:17:02 by ruidos-s         ###   ########.fr       */
+/*   Created: 2023/10/05 09:47:37 by ruidos-s          #+#    #+#             */
+/*   Updated: 2023/10/05 10:30:33 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include "libft.h"
+#include <string.h>
 
-int	main(void)
+void	*ft_memset(void *b, int c, size_t len)
 {
-	int	nbr;
-
-	nbr = ft_atoi(" 00745 8");
-	printf("%d\n", nbr);
-	printf("%d\n", ft_isalnum('5'));
-	return (0);
+	while (len--)
+		((unsigned char *)b)[len] = (unsigned char)c;
+	return (b);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -6,19 +6,21 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:37:58 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/04 14:55:50 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/05 10:33:58 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+# include <string.h>
 
-int	ft_atoi(const char *str);
-int	ft_strlen(const char *str);
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int d);
-int	ft_isprint(int c);
+int		ft_atoi(const char *str);
+int		ft_strlen(const char *str);
+int		ft_isalnum(int c);
+int		ft_isalpha(int c);
+int		ft_isascii(int c);
+int		ft_isdigit(int d);
+int		ft_isprint(int c);
+void	*ft_memset(void *b, int c, size_t len);
 
 #endif
