382efe387277c0aa42ddda94a8b88b22add478e6
ruipsmartins <rui_pedru@hotmail.com>
2023-10-06 13:03:04 +0100

    guardar coisas no github

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 12:08:29 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/06 12:19:24 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:56:52 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
+	if (!dest && !src)
+		return (0);
+	if ((char *)dest < (const char *)src + n)
+	{
+		/* code */
+	}
+	else ft_memcpy(*dest, *src, n);
 	
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/06 11:46:49 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/06 12:24:59 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,17 @@
 
 int	main(void)
 {
-	char	src[] = "Hello, World!";
-	char	dest[20];
+	char str1[] = "Geeks"; // Array of size 100 
+	char str2[] = "Quiz"; // Array of size 5 
 
-	ft_memcpy(dest, src, ft_strlen(src) + 1);
+	puts("str1 before memmove "); 
+	puts(str1); 
 
-	printf("Conteúdo da área de destino: %s\n", dest);
+	/* Copies contents of str2 to sr1 */
+	memmove(str1, str2, sizeof(str2)); 
+
+	puts("\nstr1 after memmove "); 
+	puts(str1); 
+
 	return (0);
 }
