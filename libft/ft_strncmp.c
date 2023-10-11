5a35c86907e5ca2fa6ce01452e54fc6057ab23a1
Rui Pedro Dos Santos Martins <ruidos-s@c1r9s2.42lisboa.com>
2023-10-11 11:42:28 +0100

    guardar coisas no github

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 16:08:57 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/10 16:31:42 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:41:03 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,25 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
 
+	if (n == 0)
+		return (0);
 	i = 0;
-	if (n != 0)
+	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 	{
-		while (i <= n || s1[i] != '\0' || s2[i] != '\0')
-		{
-			if (s1[i] > s2[i])
-				return (1);
-			if (s1[i] < s2[i])
-				return (-1);
+		if (i < (n - 1))
 			i++;
-		}
+		else
+			return (0);
 	}
-	return (0);
+	return ((unsigned char)(s1[i]) - (unsigned char)(s2[i]));
 }
+
+/* int	main(void)
+{
+	printf("strncmp: %d\n", strncmp("abc", "abc", 7));
+	printf("ft_strncmp: %d\n", ft_strncmp("abc", "abc", 7));
+
+	printf("strncmp: %d\n", strncmp("abcdef", "abc\375xx", 5));
+    printf("ft_strncmp: %d\n", ft_strncmp("abcdef", "abc\375xx", 5));
+	return (0);
+} */
--- libft/main.c
+++ libft/main.c
@@ -6,19 +6,17 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/10 16:48:45 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/11 11:42:18 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
 int	main(void)
 {
 	printf("strncmp: %d\n", strncmp("abc", "abc", 7));
-	printf("ft_strncmp: %d\n\n", ft_strncmp("abc", "abc", 7));
-	
+	printf("ft_strncmp: %d\n", ft_strncmp("abc", "abc", 7));
 	printf("strncmp: %d\n", strncmp("abcdef", "abc\375xx", 5));
-	printf("ft_strncmp: %d\n", ft_strncmp("abcdef", "abc\375xx", 5));
+    printf("ft_strncmp: %d\n", ft_strncmp("abcdef", "abc\375xx", 5));
 	return (0);
 }
