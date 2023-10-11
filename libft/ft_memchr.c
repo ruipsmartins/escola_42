fddcf686e9a15f7bf6e388cdc7996134678aa498
Rui Pedro Dos Santos Martins <ruidos-s@c1r7s2.42lisboa.com>
2023-10-11 15:48:20 +0100

    guardar coisas no github

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 12:03:37 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/11 12:04:24 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:47:24 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,21 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (*(unsigned char *)(s + i) == (unsigned char)c)
+			return ((void *)(s + i));
+		i++;
+	}
 	return (0);
 }
+
+/* int	main(void)
+{
+	printf("strchr: %p\n", memchr("teste", 'e', 3));
+	printf("ft_strchr: %p\n", ft_memchr( "teste", 'e', 3));
+	return (0);
+} */
--- libft/main.c
+++ libft/main.c
@@ -6,17 +6,16 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/11 12:02:20 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/11 15:38:18 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+
 int	main(void)
 {
-	printf("strncmp: %d\n", strncmp("abc", "abc", 7));
-	printf("ft_strncmp: %d\n", ft_strncmp("abc", "abc", 7));
-	printf("strncmp: %d\n", strncmp("abcdef", "abc\375xx", 5));
-	printf("ft_strncmp: %d\n", ft_strncmp("abcdef", "abc\375xx", 5));
+	printf("strchr: %p\n", memchr("teste", 'e', 3));
+	printf("ft_strchr: %p\n", ft_memchr( "teste", 'e', 3));
 	return (0);
 }
