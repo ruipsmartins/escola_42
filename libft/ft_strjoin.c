11ef1d6741af6e980ee20b71d87de9ef2391feb1
ruidos-s <ruidos-s@student.42lisboa.com>
2023-10-13 16:21:26 +0100

    guardar coisas no github

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 15:46:33 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/13 16:08:02 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/13 16:14:13 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	ft_strlcat(newstr + s1_len, s2, s2_len + 1);
 	return (newstr);
 }
+/* int	main(void)
+{
+	char *s1 = "hello ";
+	char *s2 = "world!";
+	char *newstr = ft_strjoin(s1, s2);
+
+	if (newstr != NULL)
+	{
+		printf("Substring: %s\n", newstr);
+		free(newstr);
+	}
+	else
+	{
+		printf("Failed to create substring.\n");
+	}
+
+	return (0);
+} */
