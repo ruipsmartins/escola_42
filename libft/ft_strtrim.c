cb7354e981cd4875bf1c342efb25f00bf515e58c
ruidos-s <ruidos-s@student.42lisboa.com>
2023-10-16 16:22:45 +0100

    guardar coisas no github

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 10:09:49 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/16 12:57:28 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:22:23 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (*s1 && ft_strchr(set, *s1))
 		s1++;
 	i = ft_strlen(s1);
-	while (i && ft_strchr(set, s1[i]))
+	while (ft_strchr(set, s1[i]))
 		i--;
 	return (ft_substr(s1, 0, i + 1));
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/16 13:03:03 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/16 16:19:02 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 int	main(void)
 {
-	char	*s = "ola bananas ola";
-	char	*trim = "ola";
+	char	*s = "ola mensagem a";
+	char	*trim = "alo";
 
 	printf("%s\n", ft_strtrim(s, trim));
 	return (0);
