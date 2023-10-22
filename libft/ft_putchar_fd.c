160a091a372c9c783de20f7a2d851d13a6f5d519
ruipsmartins <rui_pedru@hotmail.com>
2023-10-22 10:29:34 +0100

    guardar coisas no github

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -1,26 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/22 10:18:27 by ruidos-s         ###   ########.fr       */
+/*   Created: 2023/10/22 10:11:06 by ruidos-s          #+#    #+#             */
+/*   Updated: 2023/10/22 10:19:29 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
-#include <ctype.h>
 
-
-
-int	main(void)
+void	ft_putchar_fd(char c, int fd)
 {
-	char	c;
-
-	c = 'z';
-	ft_putchar_fd(c, 1);
-	return (0);
+	write(fd, &c, 1);
 }
