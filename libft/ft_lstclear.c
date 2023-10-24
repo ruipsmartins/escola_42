ecffe4f79b7fafffb8dcb8cdcc24d1517e63f3c8
ruidos-s <ruidos-s@student.42lisboa.com>
2023-10-24 16:59:56 +0100

    guardar coisas no github

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,8 +6,30 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 16:40:21 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/24 16:41:02 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/24 16:59:53 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_lstclear(t_list **lst, void (*del)(void *));
+/* Deletes and frees the given node and every
+successor of that node, using the function ’del’
+and free(3).
+Finally, the pointer to the list must be set to
+NULL. */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*node;
+
+	if (!*lst)
+		return ;
+	while (*lst)
+	{
+		node = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = node;
+	}
+	*lst = NULL;
+}
+//FAZER TESTES!!!!!!!!!!!!!
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/24 16:37:03 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/24 16:42:15 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,51 +17,51 @@
 
 void ft_del(void *node)
 {
-    t_list *new_node = (t_list *)node;
-    new_node->content = NULL;
+	t_list *new_node = (t_list *)node;
+	new_node->content = NULL;
 }
 
 int    main(void)
 {
-    int        a = 100;
-    int        b = 200;
-    int        c = 300;
-    t_list *lista = NULL;
+	int        a = 100;
+	int        b = 200;
+	int        c = 300;
+	t_list *lista = NULL;
 
-    t_list    *a_node = ft_lstnew(&a);
-    t_list    *b_node = ft_lstnew(&b);
-    t_list    *c_node = ft_lstnew(&c);
+	t_list    *a_node = ft_lstnew(&a);
+	t_list    *b_node = ft_lstnew(&b);
+	t_list    *c_node = ft_lstnew(&c);
 
-    ft_lstadd_front(&lista, c_node);
-    ft_lstadd_front(&lista, b_node);
-    ft_lstadd_front(&lista, a_node);
+	ft_lstadd_front(&lista, c_node);
+	ft_lstadd_front(&lista, b_node);
+	ft_lstadd_front(&lista, a_node);
 
-    t_list *current = lista;
-    while (current)
-    {
-        if (current->content)
-        {
-            printf("Conteúdo do node: %d\n", *((int *)(current->content)));
-        }
-        current = current->next;
-    }
-    printf("depois de apagar:\n");
-    ft_lstdelone(c_node, &ft_del);
-    b_node->next = NULL;
-    lista = a_node;
-    current = lista;
-    while (current)
-    {
-        if (current->content)
-        {
-            printf("Conteúdo do node: %d\n", *((int *)(current->content)));
-        }
-        current = current->next;
-    }
+	t_list *current = lista;
+	while (current)
+	{
+		if (current->content)
+		{
+			printf("Conteúdo do node: %d\n", *((int *)(current->content)));
+		}
+		current = current->next;
+	}
+	printf("depois de apagar:\n");
+	ft_lstdelone(c_node, &ft_del);
+	b_node->next = NULL;
+	lista = a_node;
+	current = lista;
+	while (current)
+	{
+		if (current->content)
+		{
+			printf("Conteúdo do node: %d\n", *((int *)(current->content)));
+		}
+		current = current->next;
+	}
 
-    free(a_node);
-    free(b_node);
-    //free(c_node);
-    return (0);
+	free(a_node);
+	free(b_node);
+	//free(c_node);
+	return (0);
 }
 
