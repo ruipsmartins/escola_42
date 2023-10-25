084b5c71b20f02c9110524f5749821cc7c232692
ruidos-s <ruidos-s@student.42lisboa.com>
2023-10-25 11:25:13 +0100

    guardar coisas no github

--- libft/ft_lstdelone.c
+++ libft/ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 12:27:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/25 11:07:39 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/25 11:25:09 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,39 +27,34 @@ void	ft_lstdelone(t_list *lst, void (*del)(void*))
 /*
 void    ft_del(void *content)
 {
-    content = NULL;
+    free(content);
 }
 
 int main(void)
 {
-    int a = 10;
-    int b = 20;
-    int c = 30;
 
-    t_list *node_a = ft_lstnew(&a);
-    t_list *node_b = ft_lstnew(&b);
-    t_list *node_c = ft_lstnew(&c);
+    t_list *node_a = ft_lstnew(ft_strdup("um"));
+    t_list *node_b = ft_lstnew(ft_strdup("dois"));
+    t_list *node_c = ft_lstnew(ft_strdup("tres"));
 
-    // Criando a lista encadeada
     node_a->next = node_b;
     node_b->next = node_c;
 
-    printf("Conteúdo antes da deleção:\n");
+    printf("Conteúdo antes da ft_lstdelone:\n");
     t_list *current = node_a;
     while (current)
     {
-        printf("%d\n", *((int *)(current->content)));
+        printf("%s\n", (char *)(current->content));
         current = current->next;
     }
 
-    // Deletando um nó específico da lista
     ft_lstdelone(node_b, &ft_del);
-	node_a->next = node_c;
-    printf("Conteúdo depois da deleção:\n");
-    //current = node_a;
+	//node_a->next = node_c;
+    printf("Conteúdo depois da ft_lstdelone:\n");
+    current = node_a;
     while (current)
     {
-        printf("%d\n", *((int *)(current->content)));
+        printf("%s\n", (char *)(current->content));
         current = current->next;
     }
 
