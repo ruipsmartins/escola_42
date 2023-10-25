ca180d3d0441a2245b73944dacd3b896cd89eead
ruidos-s <ruidos-s@student.42lisboa.com>
2023-10-25 15:37:21 +0100

    guardar coisas no github

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 13:05:41 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/25 13:13:46 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/25 15:18:11 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,21 @@ delete the content of a node if needed.*/
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list new_list;
-	
+	t_list	*new_list;
+	t_list	*new_node;
+
 	if (!lst || !f)
-		return NULL;
-	
-	return (new_list)
+		return (NULL);
+	new_list = NULL;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, (*del));
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
--- libft/main.c
+++ libft/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 17:05:34 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/10/25 12:50:28 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/10/25 14:39:35 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,22 @@ int main(void)
     t_list *node_a = ft_lstnew(ft_strdup("um"));
     t_list *node_b = ft_lstnew(ft_strdup("dois"));
     t_list *node_c = ft_lstnew(ft_strdup("tres"));
-
+	t_list	*new_list = NULL;
+    t_list *current = node_a;
     node_a->next = node_b;
     node_b->next = node_c;
 
-    printf("Conteúdo antes da ft_lstlstiter:\n");
-    t_list *current = node_a;
+    printf("Conteúdo antes da ft_lstmap:\n");
     while (current)
     {
         printf("%s\n", (char *)(current->content));
         current = current->next;
     }
-	ft_lstiter(node_a, &ft_test);
-    current = node_a;
-    printf("Conteúdo depois da ft_lstlstiter:\n");
+	//ft_lstiter(node_a, &ft_test);
+	new_list = ft_lstmap(node_a, (void *)ft_test, &free);
+
+    current = new_list;
+    printf("Conteúdo depois da ft_lstmap:\n");
     while (current)
     {
         printf("%s\n", (char *)(current->content));
@@ -50,6 +52,7 @@ int main(void)
     }
 
     ft_lstclear(&node_a, free);
+    ft_lstclear(&new_list, free);
 	
     return (0);
 }
