302bb1db9223d03e54429fc2af662c015a6fe8be
ruidos-s <ruidos-s@student.42lisboa.com>
2023-11-06 12:14:55 +0000

    enviar coisas para o github

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 15:36:27 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/11/06 09:57:24 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:36:55 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_printf(const char *format, ...)
 	va_start(ap, format);
 	count = 0;
 	while (*format)
-	{
+	{	
 		if (*format == '%')
 			count += print_format(*(++format), ap);
 		else
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 09:33:36 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/11/06 10:25:52 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/11/06 11:56:10 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,5 +29,10 @@ int	main(void)
 	count2 = ft_printf("meu_printf --> %%c=%c s=%s, u=%u, x=%x X=%X p=%p\n", c, str,
 			num, num, num, &num);
 	printf("\ncount1 = %d, count2 = %d\n", count1, count2);
+
+	count1 = printf(" NULL %p NULL \n", NULL);
+	count2 = ft_printf(" NULL %p NULL \n", NULL);
+	printf("\ncount1 = %d, count2 = %d\n", count1, count2);
+
 	return (0);
 }
