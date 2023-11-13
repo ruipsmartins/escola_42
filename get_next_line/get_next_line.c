95b8380532ed69d71db5838d9c9626777a17bc1e
ruidos-s <ruidos-s@student.42lisboa.com>
2023-11-13 14:39:24 +0000

    s

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,20 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:25:20 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/11/13 12:25:21 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/11/13 14:34:16 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char	buffer[BUFFER_SIZE + 1];
+
+	if (fd < 0 || BUFFER_size <= 0 || read(fd, buffer, 0) < 0)
+	{
+		return (NULL);
+	}
+	
+}
 
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,22 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 12:25:22 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/11/13 12:25:23 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/11/13 12:28:15 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
