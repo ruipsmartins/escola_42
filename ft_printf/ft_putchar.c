fb986720a22b80b0239db9f7112d5eb30c0700ae
ruidos-s <ruidos-s@student.42lisboa.com>
2023-11-13 10:33:53 +0000

    s

--- ft_printf/ft_putchar.c
+++ ft_printf/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/22 10:11:06 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/11/06 09:57:23 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/11/09 09:50:39 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,5 @@ descriptor. */
 
 int	ft_putchar(char c)
 {
-	write(1, &c, 1);
-	return (1);
+	return (write(1, &c, 1));
 }
