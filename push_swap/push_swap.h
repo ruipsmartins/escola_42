69f98df3953920822f523e4202ed259e6d09ddea
ruipsmartins <rui_pedru@hotmail.com>
2023-12-18 18:11:02 +0000

    s

---
+++ push_swap/push_swap.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/18 11:06:56 by ruidos-s          #+#    #+#             */
+/*   Updated: 2023/12/18 17:47:45 by ruidos-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+/* typedef struct	s_list
+{
+	int				nbr;
+	int				index;
+	struct s_list	*next;
+}				t_list;
+ */
+
+
+#endif
