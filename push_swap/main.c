2640221bcb28e4fd1119a83e2309b9f6246aed29
ruipsmartins <rui_pedru@hotmail.com>
2023-12-20 15:56:32 +0000

    guardar coisas no github

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,16 +6,26 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:07:12 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/18 17:57:05 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/12/20 15:53:52 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
+#include <stdio.h>
 
 int main (void)
 {
-	ft_putstr_fd("hello",1);
-	ft_putchar_fd('\n',1);
+	int		i;
+	char	**box = ft_split("10 15 42 98", ' ');
+
+	while (box[i])
+	{
+	printf("%s\n",box[i]);
+	free(box[i]);
+	i++;
+	}
+	if (box)
+		free(box);
+	
 	return (0);
 }
