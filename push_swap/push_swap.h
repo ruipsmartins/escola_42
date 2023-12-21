ea2984a8a3e21ac2267ae1946c0b2d99b6ab9ea8
ruipsmartins <rui_pedru@hotmail.com>
2023-12-21 17:07:07 +0000

    guardar coisas no github

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 11:06:56 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/21 16:33:56 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/12/21 17:04:04 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 
 typedef struct s_stack
 {
-	long				nbr;
+	long				content;
 	long				index;
 	struct s_stack		*next;
 	struct s_stack		*prev;
@@ -29,5 +29,6 @@ typedef struct s_stack
 
 void	ft_find_errors(char *str);
 int		ft_print_error(void);
+void	ft_lstadd_back(t_stack **lst, t_stack *new);
 
 #endif
--- push_swap/src/ft_add_stack.c
+++ push_swap/src/ft_add_stack.c
@@ -6,7 +6,44 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 15:37:43 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/21 15:37:50 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/12/21 17:06:11 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../push_swap.h"
+
+t_stack	*ft_lstnew(long content)
+{
+	t_stack	*new;
+
+	new = (t_stack *)malloc(sizeof(t_stack));
+	if (!new)
+		return (NULL);
+	new->content = content;
+	new->next = NULL;
+	return (new);
+}
+
+t_stack	*ft_lstlast(t_stack *lst)
+{
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_stack **lst, t_stack *new)
+{
+	t_stack	*last_node;
+
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	last_node = ft_lstlast(*lst);
+	last_node->next = new;
+}
