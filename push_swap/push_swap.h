f3abcfddccccaa07d4cbeea368947d97dda38f2c
ruipsmartins <rui_pedru@hotmail.com>
2023-12-30 14:14:32 +0000

    guardar coisas no github

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 11:06:56 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/30 10:51:38 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/12/30 14:08:41 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,5 +31,6 @@ void	ft_find_errors(char *str);
 int		ft_print_error(void);
 void	ft_add_stack(t_stack **lst, long num);
 void	process_and_add_to_stack(t_stack **stack_a, char *input);
+void	concatenate_and_process_args(t_stack **stack_a, int argc, char **argv);
 
 #endif
--- push_swap/src/process_string.c
+++ push_swap/src/process_string.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/30 10:50:32 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/30 10:59:48 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2023/12/30 14:13:21 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,23 @@ void	process_and_add_to_stack(t_stack **stack_a, char *input)
 		free(splited_input[i++]);
 	}
 	free(splited_input);
+}
+
+void	concatenate_and_process_args(t_stack **stack_a, int argc, char **argv)
+{
+	int		i;
+	char	*concatenated_str;
+	char	*temp_str;
+
+	i = 2;
+	concatenated_str = ft_strjoin(argv[1], " ");
+	while (i < argc)
+	{
+		temp_str = ft_strjoin(concatenated_str, argv[i++]);
+		free(concatenated_str);
+		concatenated_str = ft_strjoin(temp_str, " ");
+		free(temp_str);
+	}
+	process_and_add_to_stack(stack_a, concatenated_str);
+	free(concatenated_str);
 }
