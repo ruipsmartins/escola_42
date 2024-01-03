8f7dfbd30d62e9879b79d7f799920d2523462bac
ruipsmartins <rui_pedru@hotmail.com>
2024-01-03 17:29:22 +0000

    guardar coisas no github

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:07:12 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/01/03 17:23:39 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/01/03 17:28:41 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
-	t_stack	*tmp;
+	t_stack	*tmp_stack;
 	//t_stack	*stack_b;
 
 	stack_a = NULL;
@@ -25,24 +25,24 @@ int	main(int argc, char **argv)
 	if (argc >= 2)
 		concatenate_and_process_args(&stack_a, argc, argv);
 	//fazer print do que esta no stack a
-	tmp =  stack_a;
-	while (tmp)
+	tmp_stack =  stack_a;
+	while (tmp_stack)
 	{
 		printf("Conteúdo do node: ");
-		if (tmp->content)
+		if (tmp_stack->content)
 			{
-				printf("%li\n", tmp->content);
-				if (tmp->prev)
-					printf("prev: %li\n", tmp->prev->content);
+				printf("%li\n", tmp_stack->content);
+				if (tmp_stack->prev)
+					printf("prev: %li\n", tmp_stack->prev->content);
 				else
 					printf("prev = NULL\n");
-				if(tmp->next)
-					printf("next: %li\n", tmp->next->content);
+				if(tmp_stack->next)
+					printf("next: %li\n", tmp_stack->next->content);
 				else
 					printf("next: NULL\n");
 				printf("----------------------------\n");
 			}
-			tmp = tmp->next;
+			tmp_stack = tmp_stack->next;
 	}
 
 	while(stack_a->next)
