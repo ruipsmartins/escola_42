8022d0fe93ef1746354a83629cf29758501f4056
ruipsmartins <rui_pedru@hotmail.com>
2024-01-03 15:58:39 +0000

    guardar coisas no github

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 17:07:12 by ruidos-s          #+#    #+#             */
-/*   Updated: 2023/12/30 16:35:10 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/01/03 15:51:36 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ int	main(int argc, char **argv)
 	//t_stack	*stack_b;
 
 	stack_a = NULL;
-	if (argc < 2 || !argv[1][0])
-		ft_print_error();
+	// Perguntar ao pessoal se é para fazer print ao erro ou nao se nao tiver args
+	/*if (argc < 2 || !argv[1][0])
+		ft_print_error();*/
 	if (argc >= 2)
 		concatenate_and_process_args(&stack_a, argc, argv);
 	//fazer print do que esta no stack a
@@ -30,7 +31,18 @@ int	main(int argc, char **argv)
 		tmp =  stack_a;
 		stack_a = stack_a->next;
 		if (tmp->content)
-			printf("%li\n", tmp->content);
+			{
+				printf("%li\n", tmp->content);
+				if (tmp->prev)
+					printf("prev: %li\n", tmp->prev->content);
+				else
+					printf("prev = NULL\n");
+				if(tmp->next)
+					printf("next: %li\n", tmp->next->content);
+				else
+					printf("next: NULL\n");
+				printf("---------------------------------\n");
+			}
 		free(tmp);
 	}
 	free(stack_a);
