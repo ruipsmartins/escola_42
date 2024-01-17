2bb8c90f939a9a5a026a41c64664a05c5fc6df6e
ruipsmartins <rui_pedru@hotmail.com>
2024-01-17 15:44:24 +0000

    guardar coisas no github

--- push_swap/src/init_b_to_a.c
+++ push_swap/src/init_b_to_a.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 14:25:06 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/01/15 17:30:18 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/01/17 15:43:32 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,31 +25,31 @@ t_stack	*get_cheapest(t_stack *node)
 	return (NULL);
 }
 
-static void	set_target_b(t_stack *a, t_stack *b) //Define a function that sets for the current `a` node, its target node from stack `a`
+static void	set_target_b(t_stack *a, t_stack *b)
 {
-	t_stack	*current_a; //To store the pointer to the current `a` node
-	t_stack	*target_node; //To store the pointer of the target node for `b` node
-	long	best_match_index; //To store the "closest bigger" number so far
+	t_stack	*current_a;
+	t_stack	*target_node;
+	long	best_match_index;
 
 	while (b)
 	{
-		best_match_index = LONG_MAX; //Set the current best match to the max long
-		current_a = a; //Assign the pointer to point to the current `a` node
-		while (current_a) //While the pointer is not set to NULL
+		best_match_index = LONG_MAX;
+		current_a = a;
+		while (current_a)
 		{
 			if (current_a->content > b->content 
-				&& current_a->content < best_match_index) //Check if the `a` node's value is bigger than `b` node, && smaller than the "closest bigger" so far
+				&& current_a->content < best_match_index)
 			{
-				best_match_index = current_a->content; //Set the best match as the value in the current `a` node
-				target_node = current_a; //Set `b` node's target node pointer to point to the current `a` node
+				best_match_index = current_a->content;
+				target_node = current_a;
 			}
-			current_a = current_a->next; //Move to the next `a` node for processing
+			current_a = current_a->next;
 		}
-		if (best_match_index == LONG_MAX) //If the best match value has not changed
-			b->target_node = find_smaller(a); //Set `b` node's target node pointer to point to the smallest number in stack `a`
+		if (best_match_index == LONG_MAX)
+			b->target_node = find_smaller(a);
 		else
-			b->target_node = target_node; //If the best match value has been updated, then we have a target node for the current `b` node
-		b = b->next; //Move to the next `b` node for processing
+			b->target_node = target_node;
+		b = b->next;
 	}
 }
 
