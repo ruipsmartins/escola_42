9bbf6d3380f4a41d0e6b207ae3bd2ab9694025c2
ruipsmartins <rui_pedru@hotmail.com>
2024-02-27 12:51:50 +0000

    enviar para o github

--- so_long/src/open_map.c
+++ so_long/src/open_map.c
@@ -6,15 +6,15 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 16:49:32 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/02/22 18:39:03 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/02/27 12:50:50 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void free_map(t_data *data)
+void	free_map(t_data *data)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (data->map[i])
@@ -24,33 +24,28 @@ void free_map(t_data *data)
 
 char	**open_map(char *path)
 {
-	int 	fd;
+	int		fd;
 	char	*line;
-	char 	*map_data;
+	char	*map_data;
 	char	*holder;
 	char	**map;
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
-	{
-		ft_printf("error opening map\n");
 		exit (1);
-	}
 	map_data = ft_strdup("");
 	while (true)
 	{
- 		line = get_next_line(fd);
+		line = get_next_line(fd);
 		if (!line)
 			break ;
 		holder = map_data;
 		map_data = ft_strjoin(holder, line);
-		ft_printf("%s", line);
 		free(line);
 		free(holder);
 	}
 	map = ft_split(map_data, '\n');
 	free(map_data);
-	ft_printf("\n");
 	close(fd);
 	return (map);
 }
