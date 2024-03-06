2710c7347c95327764caf2dfd10fc66e3d7cdacf
ruipsmartins <rui_pedru@hotmail.com>
2024-03-06 19:16:47 +0000

    enviar para o github

--- so_long/src/flood_fill.c
+++ so_long/src/flood_fill.c
@@ -6,41 +6,43 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 14:35:07 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/03/06 18:48:02 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/03/06 19:15:51 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void  flood_fill(char **flooded_map, t_data *data, int player_x, int player_y)
+void  flood_fill(char **flooded_map, t_data *data, int start_x, int start_y)
 {
-	/* if (player_x < 0 || player_y < 0 || player_x >= data->map_size_x || player_y >= data->map_size_y)
-        return; */
-	if (flooded_map[player_x][player_y] == 'F' || flooded_map[player_x][player_y] == '1')
+	if (flooded_map[start_x][start_y] == 'F' || flooded_map[start_x][start_y] == '1')
         return;
-	else if (flooded_map[player_x][player_y] == 'C')
+	else if (flooded_map[start_x][start_y] == 'C')
 		data->c_count ++;
-	else if (flooded_map[player_x][player_y] == 'P')
+	else if (flooded_map[start_x][start_y] == 'P')
 		data->p_count ++;
-	else if (flooded_map[player_x][player_y] == 'E')
+	else if (flooded_map[start_x][start_y] == 'E')
 		data->e_count ++;
-    flooded_map[player_x][player_y] = 'F';
-   	flood_fill(flooded_map, data, player_x - 1, player_y);
-    flood_fill(flooded_map, data, player_x + 1, player_y);
-    flood_fill(flooded_map, data, player_x, player_y - 1);
-    flood_fill(flooded_map, data, player_x, player_y + 1);
-	
+    flooded_map[start_x][start_y] = 'F';
+   	flood_fill(flooded_map, data, start_x - 1, start_y);
+    flood_fill(flooded_map, data, start_x + 1, start_y);
+    flood_fill(flooded_map, data, start_x, start_y - 1);
+    flood_fill(flooded_map, data, start_x, start_y + 1);
 	return;
 }
 
 char **make_flooded_map(char **map, int x, int y, t_data *data)
 {
 	char **flooded_map;
+	int i;
+	int j;
+
 	flooded_map = (char **)malloc(sizeof(char *) * y);
-	for (int i = 0; i < y; ++i)
+	i = 0;
+	while (i < y)
 	{
 		flooded_map[i] = (char *)malloc(sizeof(char) * x);
-		for (int j = 0; j < x; ++j)
+		j = 0;
+		while (j < x)
 		{
 			flooded_map[i][j] = map[i][j];
 			if (map[i][j]== 'P')
@@ -48,7 +50,9 @@ char **make_flooded_map(char **map, int x, int y, t_data *data)
 				data->player_x = i;
 				data->player_y = j;
 			}
+			j++;
 		}
+		i++;
 	}
 	return (flooded_map);
 }
@@ -66,24 +70,15 @@ int flood_test(t_data *data)
 	char **flooded_map = make_flooded_map(data->map, data->map_size_x, data->map_size_y, data);
 	start_x = data->player_x;
 	start_y = data->player_y;
-
 	flood_fill(flooded_map, data, start_x, start_y);
-	/* for (int y = 0; y < data->map_size_y; y++)
-	{
-		for (int x = 0; x < data->map_size_x; x++)
-		{
-			ft_printf("%c", flooded_map[y][x]);
-		}
-			//free(flooded_map[y]);
-		ft_printf("\n");
-	} */
 	while (i < data->map_size_y)
 		free(flooded_map[i++]);
 	free(flooded_map);
-
 	if (data->c_count != data->n_collectables || data->e_count != 1 || data->p_count != 1)
+	{
+		ft_printf("não consegue apanhar todas as moedas ou sair\n");
 		return 0;
+	}
 	ft_printf("numero de C: %d de E: %d e de P:%d\n", data->c_count, data->e_count, data->p_count);
-
 	return (1);
 }
