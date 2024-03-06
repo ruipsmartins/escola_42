1bdacff56e976fdd28411e673642e6aa8ef8ef57
ruipsmartins <rui_pedru@hotmail.com>
2024-03-06 18:50:34 +0000

    enviar para o github

--- so_long/src/flood_fill.c
+++ so_long/src/flood_fill.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 14:35:07 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/03/06 18:30:36 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/03/06 18:48:02 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,6 @@ void  flood_fill(char **flooded_map, t_data *data, int player_x, int player_y)
 	return;
 }
 
-
-#include <stdio.h>
-#include <stdlib.h>
 char **make_flooded_map(char **map, int x, int y, t_data *data)
 {
 	char **flooded_map;
@@ -60,29 +57,33 @@ int flood_test(t_data *data)
 {
 	int start_x;
 	int start_y;
+	int	i;
 
 	data->c_count = 0;
 	data->p_count = 0;
 	data->e_count = 0;
+	i = 0;
 	char **flooded_map = make_flooded_map(data->map, data->map_size_x, data->map_size_y, data);
 	start_x = data->player_x;
 	start_y = data->player_y;
 
 	flood_fill(flooded_map, data, start_x, start_y);
-	for (int y = 0; y < data->map_size_y; y++)
+	/* for (int y = 0; y < data->map_size_y; y++)
 	{
 		for (int x = 0; x < data->map_size_x; x++)
 		{
-			printf("%c", flooded_map[y][x]);
+			ft_printf("%c", flooded_map[y][x]);
 		}
-			free(flooded_map[y]);
-		printf("\n");
-	}
-	printf("numero de C: %d de E: %d e de P:%d\n", data->c_count, data->e_count, data->p_count);
+			//free(flooded_map[y]);
+		ft_printf("\n");
+	} */
+	while (i < data->map_size_y)
+		free(flooded_map[i++]);
 	free(flooded_map);
 
 	if (data->c_count != data->n_collectables || data->e_count != 1 || data->p_count != 1)
 		return 0;
+	ft_printf("numero de C: %d de E: %d e de P:%d\n", data->c_count, data->e_count, data->p_count);
 
 	return (1);
 }
