7836889756a4fd50d263146b1d4482d847042174
Rui Pedro Dos Santos Martins <ruidos-s@c2r10s8.42lisboa.com>
2024-03-13 15:51:29 +0000

    enviar para o github

--- so_long/src/image_checker.c
+++ so_long/src/image_checker.c
@@ -6,69 +6,76 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 12:52:07 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/03/13 15:22:26 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/03/13 15:51:22 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-/* static int check_img(void *img)
-{
-
-} */
-
 static void exit_and_clear(t_data *data)
 {
-	ft_printf("erro na imagen W\n");
-	//mlx_destroy_window(data->mlx_ptr, data->win_ptr);
-	//mlx_destroy_image(data->mlx_ptr, data->img_player);
+	ft_printf("There is an error in the images.\n");
 	mlx_destroy_display(data->mlx_ptr);
 	free(data->mlx_ptr);
 	free_map(data);
 	exit (1);
 }
 
-int check_player_img(t_data *data)
+static void check_player_img(t_data *data)
 {
-	int checker;
-
-	checker = 0;
 	data->img_player = mlx_xpm_file_to_image(data->mlx_ptr,
 			"./img/player_w.xpm", &data->img_width, &data->img_height);
 	if (!data->img_player)
 		exit_and_clear(data);
-
 	mlx_destroy_image(data->mlx_ptr, data->img_player);
-	/* 
 	data->img_player = mlx_xpm_file_to_image(data->mlx_ptr,
 			"./img/player_a.xpm", &data->img_width, &data->img_height);
 	if (!data->img_player)
-		checker = 1;
+		exit_and_clear(data);
 	mlx_destroy_image(data->mlx_ptr, data->img_player);
 	data->img_player = mlx_xpm_file_to_image(data->mlx_ptr,
 			"./img/player_s.xpm", &data->img_width, &data->img_height);
 	if (!data->img_player)
-		checker = 1;
+		exit_and_clear(data);
 	mlx_destroy_image(data->mlx_ptr, data->img_player);
 	data->img_player = mlx_xpm_file_to_image(data->mlx_ptr,
 			"./img/player_d.xpm", &data->img_width, &data->img_height);
 	if (!data->img_player)
-		checker = 1;
-	mlx_destroy_image(data->mlx_ptr, data->img_player); */
-	return (checker);
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_player);
 }
 
-
-/* bool check_ambient_img(t_data *data, bool checker)
+static void check_ambient_img(t_data *data)
 {
-	
-
-} */
-
+	data->img_ground = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./img/fundo.xpm", &data->img_width, &data->img_height);
+	if (!data->img_ground)
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_ground);
+	data->img_wall = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./img/bricksx64.xpm", &data->img_width, &data->img_height);
+	if (!data->img_wall)
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_wall);
+	data->img_collectable = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./img/gold-coin.xpm", &data->img_width, &data->img_height);
+	if (!data->img_collectable)
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_collectable);
+	data->img_exit = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./img/exit_close.xpm", &data->img_width, &data->img_height);
+	if (!data->img_exit)
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_exit);
+	data->img_exit = mlx_xpm_file_to_image(data->mlx_ptr,
+			"./img/exit_open.xpm", &data->img_width, &data->img_height);
+	if (!data->img_exit)
+		exit_and_clear(data);
+	mlx_destroy_image(data->mlx_ptr, data->img_exit);
+}
 
 void image_checker(t_data *data)
 {
-	//check_ambient_img(data, checker);
 	check_player_img(data);
-
+	check_ambient_img(data);
 }
