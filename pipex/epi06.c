27e8212a246bf59a39d795c3a2f45af361bb4bbc
Rui Pedro Dos Santos Martins <ruidos-s@c1r19s6.42lisboa.com>
2024-04-08 15:48:23 +0100

    s

--- pipex/epi06.c
+++ pipex/epi06.c
@@ -5,15 +5,37 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+//Communicating between processes (using pipes) in C
+
 int wait();
 
 int main (int argc, char *argv[])
 {
-	int id1 = fork();
-	int id2 = fork();
+	int fd[2];
 
+	if(pipe(fd) == -1)
+	{
+		printf("error opening the pipe\n");
+		return 1;
+	}
+	int id = fork();
 
-	
+	if (id == 0)
+	{
+		close(fd[0]);
+		int x;
+		printf("input a number: ");
+		scanf("%d", &x);
+		write(fd[1], &x, sizeof(int));
+		close(fd[1]);
+	} else {
+		close(fd[1]);
+		int y;
+		read(fd[0], &y, sizeof(int));
+		close (fd[0]);
+		printf("got from child process %d\n", y);
+	}
+	int a = 65;	
 	
 	return 0;	
 }
