98d19783c2d31a259ab20f9ccd8e1c652c7cfe6e
ruipsmartins <rui_pedru@hotmail.com>
2024-04-21 17:02:06 +0100

    upload pipex

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 14:37:49 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/04/21 12:28:26 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/04/21 16:58:50 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,9 @@
 
 int	main(int ac, char **av)
 {
-	int	i;
 	int fd[2];
+	int out_file;
+	int pid;
 
 	if (ac != 5)
 	{
@@ -24,23 +25,28 @@ int	main(int ac, char **av)
 	}
 	else if (ac == 5)
 	{
-		i = 1;
-		pipe(fd);
-		int id = fork();
 
-		if (id == 0)
+		pipe(fd);
+		pid = fork();
+
+		if (pid == 0)
 		{
+			// Filho
+			close(fd[0]); // Fecha a extremidade de leitura do tubo
+			out_file = open(av[4], O_WRONLY | O_CREAT, 0777);
+			dup2(out_file, STDOUT_FILENO); // Redireciona a saída padrão para o arquivo
+			close(out_file); // Fecha o arquivo
+			close(fd[1]); // Fecha a extremidade de escrita do tubo
 			execve("/bin/bash", (char *[]){"bash", "-c", av[2], NULL}, NULL);
+			exit(0);
+
 		}
-		else if (id != 0)
+		else if (pid != 0)
 		{
+			close(fd[0]);
+			close(fd[1]);
 			wait(NULL);
-			while (i < ac)
-			{
-				ft_printf(av[i]);
-				ft_printf("\n");
-				i++;
-			}
+			ft_printf("programa acabado \n");
 		}	
 	}
 	return (0);
