fa5c812b71ce3f60dc6511ca70c3b6dcc2706e42
Rui Pedro Dos Santos Martins <ruidos-s@c2r10s8.42lisboa.com>
2024-04-22 13:11:45 +0100

    upload pipex

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 14:37:49 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/04/21 16:58:50 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/04/22 12:40:25 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,31 @@ int	main(int ac, char **av)
 	}
 	else if (ac == 5)
 	{
-
 		pipe(fd);
 		pid = fork();
 
 		if (pid == 0)
 		{
 			// Filho
-			close(fd[0]); // Fecha a extremidade de leitura do tubo
+			close(fd[0]);	// Fecha a extremidade de leitura do pipe
 			out_file = open(av[4], O_WRONLY | O_CREAT, 0777);
-			dup2(out_file, STDOUT_FILENO); // Redireciona a saída padrão para o arquivo
-			close(out_file); // Fecha o arquivo
-			close(fd[1]); // Fecha a extremidade de escrita do tubo
+			
+			dup2(out_file, STDOUT_FILENO);	// Redireciona a saída padrão para o arquivo
 			execve("/bin/bash", (char *[]){"bash", "-c", av[2], NULL}, NULL);
+			fd[1] = out_file;
+			close(out_file);	// Fecha o arquivo
+			close(fd[1]);	// Fecha a extremidade de escrita do pipe
 			exit(0);
 
 		}
 		else if (pid != 0)
 		{
-			close(fd[0]);
-			close(fd[1]);
+			close(fd[1]);	// Fecha a extremidade de escrita do pipe
+			ft_printf("%s", fd[0]);
+			close(fd[0]);	// Fecha a extremidade de leitura do pipe
 			wait(NULL);
-			ft_printf("programa acabado \n");
 		}	
+			ft_printf("programa acabado! \n");
 	}
 	return (0);
 }
