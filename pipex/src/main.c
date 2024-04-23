114773261aa5ad633744e7b0959c819eaae6caf6
Rui Pedro Dos Santos Martins <ruidos-s@c1r19s6.42lisboa.com>
2024-04-23 15:50:32 +0100

    upload pipex

--- pipex/src/main.c
+++ pipex/src/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 14:37:49 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/04/23 12:42:00 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:16:48 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,8 @@ int	main(int ac, char **av)
 			ft_child(av, fd);
 		else
 			ft_parent(av, fd);
+		close(fd[0]);
+		close(fd[1]);
 	}
 	else
 	{
--- pipex/src/utils.c
+++ pipex/src/utils.c
@@ -6,25 +6,37 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 10:08:20 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/04/23 12:40:28 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:14:33 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
+
 void	ft_child(char **av, int *fd)
 {
 	int	in_file;
 
+	if (access(av[1], F_OK) == -1)
+	{
+		ft_printf("zsh: no such file or directory: %s\n", av[1]);
+		exit(1);
+	}
+	if (access(av[1], R_OK) == -1)
+	{
+		ft_printf("zsh: permission denied: %s\n", av[1]);
+		exit(1);
+	}
 	in_file = open(av[1], O_RDONLY);
 	if (in_file == -1)
 	{
-		ft_printf("zsh: no such file or directory: %s\n", av[01]);
+		perror("open infile");
 		exit(1);
 	}
 	close(fd[0]);
 	dup2(fd[1], STDOUT_FILENO);
 	dup2(in_file, STDIN_FILENO);
+	close(in_file);
 	close(fd[1]);
 	execve("/bin/zsh", (char *[]){"zsh", "-c", av[2], NULL}, NULL);
 }
@@ -36,6 +48,11 @@ void	ft_parent(char **av, int *fd)
 	wait(NULL);
 	close(fd[1]);
 	out_file = open(av[4], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	if (out_file == -1)
+	{
+		perror("open outfile");
+		exit(1);
+	}
 	dup2(out_file, STDOUT_FILENO);
 	dup2(fd[0], STDIN_FILENO);
 	close(out_file);
