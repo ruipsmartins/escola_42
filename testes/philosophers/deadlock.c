f2c46d79d8f0bfd491a0bb4aa47fbc208b1bd7fa
Rui Pedro Dos Santos Martins <ruidos-s@c1r15s15.42lisboa.com>
2024-05-02 15:21:04 +0100

    philo stuff

--- testes/philosophers/deadlock.c
+++ testes/philosophers/deadlock.c
@@ -7,13 +7,26 @@ typedef struct s_cowboy
 	pthread_t	thread;
 }				t_cowboy;
 
+void	*action(void *data)
+{
+	t_cowboy cowboy;
+
+	cowboy = *(t_cowboy *)data;
+	
+	return NULL;
+}
+
 
 int main()
 {
 	t_cowboy	ugly = {"ugly"};
 	t_cowboy	bad = {"bad"};
 
+	pthread_create(&ugly.thread, NULL, action, &ugly);
+	pthread_create(&bad.thread, NULL, action, &bad);
 
+	pthread_join(ugly.thread, NULL);
+	pthread_join(bad.thread, NULL);
 
 	return 0;
 }
