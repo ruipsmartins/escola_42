537e608a4e1817a608bc865458f98c33b42e8d18
Rui Pedro Dos Santos Martins <ruidos-s@c1r15s15.42lisboa.com>
2024-05-02 14:36:10 +0100

    philo stuff

---
+++ testes/philosophers/race_condition.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <stdint.h>
+
+uint64_t ledger = 0;
+
+pthread_mutex_t ledger_lock;
+
+void *update_ledger(void *data)
+{
+	pthread_mutex_lock(&ledger_lock);
+	ledger += 1;
+	pthread_mutex_unlock(&ledger_lock);
+	return NULL;
+}
+
+int main()
+{
+	pthread_t	baristas[1000];
+
+	pthread_mutex_init(&ledger_lock, NULL);
+
+	for (size_t i = 0; i < 1000; i++)
+	{
+	if (pthread_create(baristas + i, NULL, update_ledger, NULL))
+		exit(EXIT_FAILURE);
+	}
+	
+
+	for (size_t i = 0; i < 1000; i++)
+	{
+		pthread_join(baristas[i],NULL);
+	}
+
+	printf("%lu\n", ledger);
+	
+	return 0;
+}
---
+++ testes/philosophers/starbucks.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void *make_coffee(void *data)
+{
+	printf("Barista preparing coffee \n");
+	sleep(2);
+	printf("coffee ready!\n");
+	sleep(10);
+	return NULL;
+}
+
+int main()
+{
+	pthread_t	baristas[50];
+
+
+	for (size_t i = 0; i < 50; i++)
+	{
+	pthread_create(baristas + i, NULL, make_coffee, NULL);
+	printf("Identifier -> %lu\n",baristas[i]);
+	}
+	
+
+	for (size_t i = 0; i < 50; i++)
+	{
+		pthread_join(baristas[i],NULL);
+	}
+	
+	return 0;
+}
