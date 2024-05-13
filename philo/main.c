91854c0705d560afbbf0de54bc9678f0f067cf74
ruipsmartins <rui_pedru@hotmail.com>
2024-05-13 17:38:55 +0100

    update philo

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 16:30:36 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/05/09 16:39:08 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/05/13 16:55:36 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	main(int ac, char **av)
 {
 	if (ac > 0)
 	{
-		write(1, "hello\n", strlen("hello\n"));
+		write(1, "hello\n",6);
 		printf("hello %s!\n", av[0]);
 	}
 	
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: ruidos-s <ruidos-s@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 16:30:31 by ruidos-s          #+#    #+#             */
-/*   Updated: 2024/05/09 16:58:40 by ruidos-s         ###   ########.fr       */
+/*   Updated: 2024/05/13 17:38:26 by ruidos-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,40 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <threads.h>
+#include <pthread.h>
 #include <sys/time.h>
+#include <stdbool.h>
+
+typedef struct s_fork
+{
+	pthread_mutex_t fork;
+	int				fork_id;
+	
+}					t_fork;
+
+
+typedef struct s_philo
+{
+	int		id;
+	long	meals_counter;
+	bool	full;
+	long	last_meal_time;
+	t_fork	*left_fork;
+	t_fork	*right_fork;
+	pthread_t thread_id;
+	
+}		t_philo;
+
+typedef struct s_table
+{
+	long		philo_number;
+	long		time_to_die;
+	long		time_to_eat;
+	long		time_to_sleep;
+	long		nbr_limit_meals;
+}				t_table;
+
+
 
 
 
